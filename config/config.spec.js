08e0328e36cdf55eb218421a77e1027d16eb7388
Andre Torres <atorres@devsu.com>
2020-06-15 23:47:21 -0500

    setup middelware and cheerio library

--- config/config.spec.js
+++ config/config.spec.js
@@ -1,47 +1,53 @@
 describe('config', () => {
-   let config;
- 
-   describe('without env variables', () => {
-     beforeEach(() => {
-       config = require('./config');
-     });
- 
-     test('should return default values', () => {
-       expect(config).toEqual(expect.objectContaining({
-         'app': expect.objectContaining({
-           'port': expect.any(Number),
-           'path': '/api',
-         }),
-       }));
-     });
-   });
- 
-   describe('with env variables', () => {
-     let initialEnv;
- 
-     /* eslint-disable */
-     beforeEach(() => {
-       initialEnv = process.env;
-       process.env.APP_PORT = '4000';
-       process.env.APP_PATH = '/whatever';
-       jest.resetModules();
-       config = require('./config');
-     });
-     /* eslint-enable */
- 
-     afterEach(() => {
-       process.env = initialEnv;
-       jest.resetModules();
-     });
- 
-     test('should return env variables values', () => {
-       expect(config).toEqual(expect.objectContaining({
-         'app': expect.objectContaining({
-           'port': 4000,
-           'path': '/whatever',
-         }),
-       }));
-     });
-   });
- });
- 
+  let config;
+
+  describe('without env variables', () => {
+    beforeEach(() => {
+      config = require('./config');
+    });
+
+    test('should return default values', () => {
+      expect(config).toEqual(expect.objectContaining({
+        'app': expect.objectContaining({
+          'port': expect.any(Number),
+          'path': '/api',
+        }),
+        'crawler': expect.objectContaining({
+          'url': 'https://news.ycombinator.com/',
+        }),
+      }));
+    });
+  });
+
+  describe('with env variables', () => {
+    let initialEnv;
+
+    /* eslint-disable */
+    beforeEach(() => {
+      initialEnv = process.env;
+      process.env.APP_PORT = '4000';
+      process.env.APP_PATH = '/whatever';
+      process.env.WEB_CRAW_URL = 'https://google.com/';
+      jest.resetModules();
+      config = require('./config');
+    });
+    /* eslint-enable */
+
+    afterEach(() => {
+      process.env = initialEnv;
+      jest.resetModules();
+    });
+
+    test('should return env variables values', () => {
+      expect(config).toEqual(expect.objectContaining({
+        'app': expect.objectContaining({
+          'port': 4000,
+          'path': '/whatever',
+        }),
+        'crawler': expect.objectContaining({
+          'url': 'https://google.com/',
+        }),
+      }));
+    });
+  });
+});
